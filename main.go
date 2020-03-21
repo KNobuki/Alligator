c2e763501e26dcae045ecb7cefb8c2d27041a883
KNobuki <nobuki171532@gmail.com>
2020-03-21 16:12:04 +0900

    added hundler func

--- main.go
+++ main.go
@@ -7,25 +7,6 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-var users map
-
-type User struct {
-	UserID string
-	// Password string
-	// Mail     string
-	Items []Item
-}
-
-type Item struct {
-	ItemID    string
-	ImagePath string
-	User      *User
-}
-
-func retrieve (userid string, users []User) (User, error) {
-	return users[userid], nil
-}
-
 func showTop(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "TopPage!\n")
 }
@@ -39,25 +20,38 @@ func showLogIn(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func showMyPage(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	user := retrieve()
-	fmt.Fprintf(w, "hello, %s !\n", p.ByName("userid"))
+	fmt.Fprintf(w, "%sのページ\n", p.ByName("userid"))
+}
+
+func registUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintf(w, "%sを登録しました!\n", p.ByName("userid"))
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintf(w, "%sを削除しました!\n", p.ByName("userid"))
+}
+
+func updateUserInfo(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintf(w, "%sの情報を更新しました!\n", p.ByName("userid"))
 }
 
 func showItem(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintf(w, "hello, %sの%s !\n", p.ByName("userid"), p.ByName("itemid"))
+	fmt.Fprintf(w, "%sの%sのページ\n", p.ByName("userid"), p.ByName("itemid"))
+}
+
+func uploadItem(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintf(w, "%sが%sを登録しました\n", p.ByName("userid"), p.ByName("itemid"))
+}
+
+func deleteItem(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintf(w, "%sが%sを削除しました\n", p.ByName("userid"), p.ByName("itemid"))
+}
+
+func updateItemInfo(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintf(w, "%sの%sのページ\n", p.ByName("userid"), p.ByName("itemid"))
 }
 
 func main() {
-
-	users = make(map[string] User)
-	user := User{}
-	item := Item{}
-	user.UserID = "testID"
-	item.ImagePath = "/example"
-	item.ItemID = "testItemID"
-	item.User = &user
-	user.Items = append(user.Items, item)
-	users[user.UserID] = user
 
 	mux := httprouter.New()
 
@@ -65,16 +59,16 @@ func main() {
 	mux.GET("/signup", showSignUp)
 	mux.GET("/login", showLogIn)
 	mux.GET("/UserPage/:userid", showMyPage)
-	// mux.POST("/:userid", registUser)
-	// mux.DELETE("/:userid", deleteUser)
-	// mux.PUT("/:userid/", updateUserInfo)
+	mux.POST("/:userid", registUser)
+	mux.DELETE("/:userid", deleteUser)
+	mux.PUT("/:userid/", updateUserInfo)
 	mux.GET("/UserPage/:userid/:itemid", showItem)
-	// mux.POST("/:userid/:itemid", uploadItem)
-	// mux.DELETE("/:userid/:itemid", deleteItem)
-	// mux.PUT("/:userid/:itemid", updateItemInfo)
+	mux.POST("/:userid/:itemid", uploadItem)
+	mux.DELETE("/:userid/:itemid", deleteItem)
+	mux.PUT("/:userid/:itemid", updateItemInfo)
 
 	server := http.Server{
-		Addr:    "127.0.0.1:8083",
+		Addr:    "127.0.0.1:8080",
 		Handler: mux,
 	}
 	server.ListenAndServe()
